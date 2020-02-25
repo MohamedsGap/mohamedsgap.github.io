eb6c421f54a338cec649b6d0827f9414d82bfed1
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-25 17:15:59 +0200

    Update bucket-list

--- src/pages/bucket-list/index.js
+++ src/pages/bucket-list/index.js
@@ -16,20 +16,20 @@ function BucketList() {
               <li>Build more side projects</li>
               <li>Finish BSc in Computer Science</li>
               <li>Complete my grad project successfully</li>
-              <li>Get my certification of Front end path from OpenClassrooms</li>
+              <li>Complete Front End path on OpenClassrooms successfully</li>
               <li>Work as SWE at big tech company</li>
             </ul>
             <h3>In Progress</h3>
             <ul>
               <li>Study Front End path at OpenClassrooms</li>
               <li>Work on my grad project related to Blockchain</li>
-              <li>Study Udacity Bertelsmann Challenge Cloud</li>
+              <li>Enroll in Udacity Bertelsmann Challenge | Cloud Track </li>
               <li>Take part in Huawei ICT Competition 2019-2020</li>
             </ul>
             <h3>Done</h3>
             <ul>
               <li>Accepted in Developer Circles from Facebook</li>
-              <li>Completed Udacity Front-End Nanodegree</li>
+              <li>Completed Udacity Front End Nanodegree</li>
               <li>Started studying computer science at Benha university</li>
             </ul>
           </div>
@@ -39,4 +39,65 @@ function BucketList() {
   );
 }
 
+
 export default BucketList;
+
+/*
+const internetProfiles = {
+  github: {
+    label: 'GitHub',
+    href: 'https://github.com/mohamedsgap',
+  },
+  linkedin: {
+    label: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/mohamedsgap',
+  },
+  medium: {
+    label: 'Medium',
+    href: 'https://medium.com/@MohamedsGap',
+  },
+  twitter: {
+    label: 'Twitter',
+    href: 'https://twitter.com/mohamedsgap',
+  },
+  instagram: {
+    label: 'Instagram',
+    href: 'https://www.instagram.com/mohamedsgap/',
+  },
+  facebook: {
+    label: 'Facebook',
+    href: 'https://www.facebook.com/ImMohamedAbdelNasser',
+  },
+};
+
+*/
+
+/*
+module.exports = {
+  themeConfig: {
+    footer: {
+      style: 'dark',
+      links: [
+        {},
+        {
+          title: 'Professional',
+          items: [
+            internetProfiles.github,
+            internetProfiles.medium,
+            internetProfiles.linkedin,
+          ],
+        },
+        {
+          title: 'Social',
+          items: [
+            internetProfiles.instagram,
+            internetProfiles.twitter,
+            internetProfiles.facebook,
+          ],
+        },
+        {},
+      ],
+    }
+  }
+}
+*/
