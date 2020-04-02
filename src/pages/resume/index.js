2e500d2aa4caca30305418e8a93ebfb6e5a5d9b6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-04-02 21:12:39 +0200

    Update my resume

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Layout from '@theme/Layout';
-import styles from './style.module.css';
+import React from "react";
+import Layout from "@theme/Layout";
+import styles from "./style.module.css";
 
 function Resume() {
   return (
@@ -9,7 +9,7 @@ function Resume() {
         <div className="row">
           <div className="col col--8 col--offset-2 markdown">
             <h1 className="center page-title">Resume</h1>
-            <h2>Internet Profiles</h2>
+            <h2>Mohamed on Internet</h2>
             <p>
               <a
                 href="https://github.com/mohamedsgap"
@@ -17,40 +17,40 @@ function Resume() {
                 rel="noopener"
               >
                 GitHub
-              </a>{' '}
-              &middot;{' '}
+              </a>{" "}
+              &middot;{" "}
               <a
                 href="https://www.instagram.com/mohamedsgap/"
                 target="_blank"
                 rel="noopener"
               >
                 Instagram
-              </a>{' '}
-              &middot;{' '}
+              </a>{" "}
+              &middot;{" "}
               <a
                 href="https://www.linkedin.com/in/mohamedsgap"
                 target="_blank"
                 rel="noopener"
               >
                 LinkedIn
-              </a>{' '}
-              &middot;{' '}
+              </a>{" "}
+              &middot;{" "}
               <a
                 href="https://medium.com/@MohamedsGap"
                 target="_blank"
                 rel="noopener"
               >
                 Medium
-              </a>{' '}
-              &middot;{' '}
+              </a>{" "}
+              &middot;{" "}
               <a
                 href="https://facebook.com/ImMohamedAbdelNasser"
                 target="_blank"
                 rel="noopener"
               >
                 Facebook
-              </a>{' '}
-              &middot;{' '}
+              </a>{" "}
+              &middot;{" "}
               <a
                 href="https://twitter.com/mohamedsgap"
                 target="_blank"
@@ -62,62 +62,163 @@ function Resume() {
 
             <h2>Technical Skills</h2>
             <ul className={styles.skillsList}>
-              <li><strong>Languages:</strong> JavaScript, Python, HTML/CSS, SQL, C++</li>
-              <li><strong>Frameworks and Libraries:</strong> React.JS, React Native, Redux, Express.JS, Bootstrap, jQuery, Jest</li>
-              <li><strong>Tools:</strong> Git, Webpack, Gulp.JS, Markdown</li>
-              <li><strong>Others:</strong> Cloud/AWS, Bash, Linux</li>
+              <li>
+                <strong>Languages:</strong> JavaScript, Python, HTML/CSS, SQL,
+                C++
+              </li>
+              <li>
+                <strong>Frameworks and Libraries:</strong> React.JS, React
+                Native, Redux, Next.JS, Express.JS, Bootstrap, Jest
+              </li>
+              <li>
+                <strong>Tools:</strong> Git, Webpack, Gulp.JS, BabelJS, Markdown
+              </li>
+              <li>
+                <strong>Others:</strong> Cloud/AWS, Basics of Docker and
+                Kubernetes, Linux
+              </li>
             </ul>
 
             <h2>Education</h2>
             <h3>University of Benha</h3>
             <ul>
-              <li>2016 - 2020</li>
-              <li>BSc in Computer Science.</li>
-              <li>GPA: 3.0/4.0</li>  
+              <li>
+                Pursuing a Bachelor's degree in Computer Science | 2016-2020
+              </li>
+              <li>GPA of 3.00/4 (as of July 2019)</li>
             </ul>
             <p>
-              <strong>Relevant coursework:</strong> Data Structures, Object Oriented Programming,
-              Algorithm Design and Analysis, Introduction to Computer Architecture and
-              Assembly Language, Operating Systems, Artificial Intelligence, Image
-              Processing, Networking, Compiler Design.
+              <strong>Relevant coursework:</strong> Data Structures, Object
+              Oriented Programming, Algorithm Design and Analysis, Introduction
+              to Computer Architecture and Assembly Language, Operating Systems,
+              Artificial Intelligence, Image Processing, Networking, Compiler
+              Design.
             </p>
-            <h3>OpenClassrooms</h3>
-            <p>2019 - 2020</p>
-            <p>
-              Currently studying Front End Developer path on OpenClassrooms,
-              Europe’s largest online education platform.
-            </p>
+            <h3>OpenClassrooms, Europe’s largest online education platform.</h3>
             <ul>
-              <li>Learn to analyze projects, design technical architecture, create web projects,
-              query databases, use tools simplify front-end code, implement tests, write
-              project docs, optimize performance and create responsive interfaces.
+              <li>
+                Pursuing a Bachelor’s-level diploma, Front-End Developer path |
+                June 2019 - September 2020
               </li>
               <li>
-              Take part in professional projects and mentorship opportunities.
-            </li>
+                Learn to analyze projects, design technical architecture, create
+                web projects, query databases, use tools simplify front-end
+                code, implement tests, write project docs, optimize performance
+                and create responsive interfaces.
+              </li>
+              <li>
+                Take part in professional projects and weekly mentorship
+                sessions.
+              </li>
+            </ul>
+            <h3>Udacity, Cloud DevOps Nanodegree</h3>
+            <ul>
+              <li>
+                Enroll in Udacity Cloud DevOps Nanodegree Program | March 2020 -
+                September 2020
+              </li>
+              <li>Design and deploy infrastructure as code</li>
+              <li>Deploy scaleable microservices using Kubernetes.</li>
+              <li>
+                Build and monitor CI/CD pipelines for different deployment
+                strategies
+              </li>
+            </ul>
+            <h3>Udacity, Front-End Developer Nanodegree</h3>
+            <ul>
+              <li>
+                Completed Udacity Front-End Nanodegree Program | November 2017 -
+                August 2018
+              </li>
+              <li>
+                Built and developed a variety of websites and applications
+              </li>
+              <li>
+                Connected web applications to backend server data using
+                JavaScript
+              </li>
+              <li>
+                Built competency automating application build and deployment
+                using Webpack
+              </li>
             </ul>
 
-            <h2>Experience | Work/Projects</h2>
-            <h3>Web Developement Training, Developer Circles from Facebook</h3>
-            <em>June 2019 - August 2019</em>
+            <h2>Experience</h2>
+            <h3>Front End Engineer at Valoro</h3>
+            <em>March 2020 - Present</em>
+            <p>
+              Got an opportunity to work as Part Time Front End Engineer to make
+              the user experience as rich and interactive as possible.
+            </p>
+
+            <h3>Open Source Contribution</h3>
+            <span>Passionate for all things web and open-source.</span>
+
             <ul>
-              <li>Bringing mock-ups to life in a beautiful and consistent manner</li>
-              <li>Using best practices to structure HTML/CSS and JavaScript code as components</li>
-              <li>Writing tests to ensure the site’s front-end accounts for all possible uses</li>
-              <li>Ensuring the maintenance and growth of a site’s front-end</li>
-              <li>Optimizing page loading time</li>
-            </ul>    
-            <h3>Front End Developer Nanodegree Program, Udacity</h3>
-            <em>February 2018 - August 2018</em>
-            <ul>
-              <li>Learnt HTML, the foundational structure of all websites. Style with CSS and build my own personal portfolio website.</li>
-              <li>Used the JavaScript programming language. Start working with common developer tools, including the Git version control system.</li>
-              <li>Learnt about Accessibility, Object-Oriented JavaScript techniques, closures, the "this" keyword, and the new ES6 specification.</li>
-              <li>Used APIs to fetch data for my applications. Begin building with Front End frameworks and build offline capable websites.</li>
-              <li>Leverage the power of the React UI library. Convert an existing application to use React and build my own single page React app.</li>
-            </ul>  
+              <li>
+                Completed successfully participating at Hacktoberfest’19, by
+                creating five pull requests into different open source projects
+                on GitHub, and the pull requestshave been merged successfully.
+              </li>
+              <li>
+                Contributed to React Native Website, I contributed 18 times on
+                facebook/reactnative-website, for writing a document for the new
+                “DevSetting” API which is available now in React Native 0.62, I
+                recently contributed to update some of the existing API which
+                they are mention on component drive doc issue that is opened by
+                React core team member.
+              </li>
+              <li>
+                Contributed to facebook/docusaurus, It is an open source project
+                to easily maintain and write documentation, I contributed to
+                update its documentation for how the users can use Travis CI as
+                CI/CD in their “Docusaurus” Project.
+              </li>
+              <li>
+                Core Contributor at React community and its localization, I
+                contribute to React Arabic Localization for helping the Arab
+                developers to learn React faster.
+              </li>
+              <li>
+                I recently became a member of Kubernetes organization, because I
+                kick start the Kubernetes localization in Arabic, for helping
+                the Arabic native speaker developers to easily adapt Kubernetes
+                and read an Arabic documentation
+              </li>
+              <li>
+                I am also a member of an organization called “SoftVenue”, we
+                build tools and localize some docs for helping young developers.
+              </li>
+            </ul>
 
             <h2>Awards and Achievements</h2>
+            <div className="row">
+              <div className="col col--2">
+                <h3>2020</h3>
+              </div>
+              <div className="col">
+                <ul>
+                  <li>
+                    Selected as one of top 9 students out of total 500 students who participate in National Exam of Huawei ICT competition, 
+                    I Have an opportunity to compete on Platinum and diamond medal and reach to final in China.
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col col--2">
+                <h3>2020</h3>
+              </div>
+              <div className="col">
+                <ul>
+                  <li>
+                    Selected as one of the 15% top high-performers at the first 4-months of Udacity
+                    Bertelsmann Challenge Scholarship of Cloud track, then Bertelsmann SE & CO.
+                    funded my 6-months Cloud DevOps Nanodegree at Udacity
+                  </li>
+                </ul>
+              </div>
+            </div>
             <div className="row">
               <div className="col col--2">
                 <h3>2019</h3>
@@ -125,8 +226,9 @@ function Resume() {
               <div className="col">
                 <ul>
                   <li>
-                  Selected as one of the accepted 53 applicants worldwide to pursue 12-months 
-                  Front End Bachelor-level Diploma at OpenClassrooms, fully funded by Facebook.
+                    Selected as one of the accepted 53 applicants worldwide to
+                    pursue 12-months Front End Bachelor-level Diploma at
+                    OpenClassrooms, fully funded by Facebook.
                   </li>
                 </ul>
               </div>
@@ -138,11 +240,12 @@ function Resume() {
               <div className="col">
                 <ul>
                   <li>
-                    Selected as one of the 10% top high-performers at the first 3-months of Google Developer 
-                    Challenge Scholarship, then Google funded my 6-months Front End Nanodegree at Udacity
+                    Selected as one of the 10% top high-performers at the first
+                    3-months of Google Developer Challenge Scholarship, then
+                    Google funded my 6-months Front End Nanodegree at Udacity
                   </li>
-                </ul>  
-              </div>      
+                </ul>
+              </div>
             </div>
           </div>
         </div>
